c9500dd9881d741141c357aaef92985c07e7cbf3
Daniel Keyes <danieljkeyes@gmail.com>
2017-01-11 03:17:49 +0100

    added player that does absolutely nothing

---
+++ src/noop/RobotPlayer.java
@@ -0,0 +1,12 @@
+package noop;
+
+import battlecode.common.RobotController;
+
+public strictfp class RobotPlayer {
+
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) {
+        while(true);
+    }
+
+}
